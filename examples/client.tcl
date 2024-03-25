db83c9e25735a98ded49136bf8f00faa078d1623
Massimo Manghi <mxmanghi@apache.org>
2024-03-25 15:50:07 +0100

    restore client.tcl,server.tcl in their original form

--- examples/client.tcl
+++ examples/client.tcl
@@ -1,49 +1,15 @@
-#!/usr/bin/env tclsh
-#
-#   client.tcl sends a 'hello, world' message to the server. 
-#   This procedure can be loaded into the shell and used as a playground
-#   for examining fully asynchronous handling of messages, events and errors
-#
-#   sending an "EXIT" messages causes the server to shutdown. This can be 
-#   used to examine the event sequence brought about by a server shutdown
-#
-
-lappend auto_path .
+#!/usr/bin/env tclsh8.5
 
 package require unix_sockets
 
 proc readable {con} {
-    if {[chan eof $con]} { 
-        puts "eof detected"
-        chan close $con
-        return
-    }
-
-    set msg [chan gets $con]
+    set msg [gets $con]
     puts "got response: ($msg)"
-    incr ::done
+    set ::done 1
 }
 
-# open_channel
-#
-# creates a channel connected to the server socket and
-# returns the identification of such channel
-
-proc open_channel {} {
-    set con [unix_sockets::connect /tmp/example.socket]
-    chan event $con readable [list readable $con]
-    return $con
-}
-
-proc send_to_server {chanid msg} {
-
-    chan puts $chanid $msg
-    chan flush $chanid
-    vwait ::done
-
-}
-
-set con [open_channel]
-send_to_server $con "hello, world"
-
-chan close $con
+set con     [unix_sockets::connect /tmp/example.socket]
+puts $con "hello, world"
+flush $con
+chan event $con readable [list readable $con]
+vwait ::done
---
+++ examples/server-asynch.tcl
@@ -0,0 +1,64 @@
+#!/usr/bin/env tclsh
+#
+# modified version of server.tcl for the purpose of having a
+# more accurate handling of Tcl asynchronous I/O
+#
+#  -- fine grained control of events and error condition
+#  -- server orderly shuts down when the string "EXIT" is received
+#     generating events on the client side that need to be handled
+#     (see examples/client-asynch.tcl)
+#
+
+lappend auto_path .
+
+package require unix_sockets
+
+proc readable {con} {
+
+    if {[chan eof $con]} {
+        puts "eof detected on channel $con"
+        chan close $con
+        return
+    }
+
+    if {[catch {gets $con msg} e einfo]} {
+
+        puts stderr "error detected on 'gets <channel>': $e"
+        chan close $con
+
+    } elseif {$e > 0}  {
+
+	    puts stderr "Got $e chars in message \"$msg\" from $con, echoing"
+        chan puts $con $msg
+        chan flush $con
+
+        # this command shuts down the server
+        if {$msg == "EXIT"} {
+            incr ::wait_for_events
+        }
+
+
+    } else {
+
+        # this might happen when the client issues a chan close command
+        # but no data were in the socket buffer
+
+        puts stderr "empty line on read, ignoring"
+    }
+}
+
+proc accept {con} {
+	puts stderr "Accepting connection on $con"
+    chan event $con readable [list readable $con]
+}
+
+
+set listen [unix_sockets::listen /tmp/example.socket accept]
+
+puts stderr "server listening on socket '$listen'"
+
+vwait ::wait_for_events
+
+puts "server shut down"
+
+chan close $listen
